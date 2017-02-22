5981e514154eac387266bf591a19c5262b6f34ae
Richard Barnes <rlb@ipv.sx>
2017-02-22 14:16:35 -0800

    Add an object that represents a Merkle tree 'frontier'

---
+++ frontier.go
@@ -0,0 +1,88 @@
+package loggerhead
+
+import (
+	"crypto"
+	_ "crypto/sha256"
+)
+
+// Merkle tree primitives
+const hash = crypto.SHA256
+
+var hashCounter = 0
+
+func leafHash(d []byte) []byte {
+	h := hash.New()
+	h.Write([]byte{0x00})
+	h.Write(d)
+	return h.Sum(nil)
+}
+
+func pairHash(d1, d2 []byte) []byte {
+	h := hash.New()
+	h.Write([]byte{0x01})
+	h.Write(d1)
+	h.Write(d2)
+
+	hashCounter += 1
+	return h.Sum(nil)
+}
+
+type frontierEntry struct {
+	SubtreeSize uint64
+	Value       []byte
+}
+
+type frontier []frontierEntry
+
+func (f *frontier) Compact() {
+	if len(*f) < 2 {
+		return
+	}
+
+	n := len(*f)
+	for n > 1 {
+		last := (*f)[n-1]
+		nextToLast := (*f)[n-2]
+
+		if last.SubtreeSize != nextToLast.SubtreeSize {
+			break
+		}
+
+		n -= 1
+		(*f)[n-1] = frontierEntry{
+			SubtreeSize: last.SubtreeSize + nextToLast.SubtreeSize,
+			Value:       pairHash(nextToLast.Value, last.Value),
+		}
+	}
+
+	(*f) = (*f)[:n]
+}
+
+func (f *frontier) Add(v []byte) {
+	leaf := leafHash(v)
+	*f = append(*f, frontierEntry{1, leaf})
+	f.Compact()
+}
+
+func (f frontier) Size() uint64 {
+	size := uint64(0)
+	for _, entry := range f {
+		size += entry.SubtreeSize
+	}
+	return size
+}
+
+func (f frontier) Head() []byte {
+	if len(f) == 1 {
+		return f[0].Value
+	}
+
+	n := len(f) - 2
+	curr := pairHash(f[n].Value, f[n+1].Value)
+	for n > 0 {
+		n -= 1
+		curr = pairHash(f[n].Value, curr)
+	}
+
+	return curr
+}
---
+++ frontier_test.go
@@ -0,0 +1,49 @@
+package loggerhead
+
+import (
+	"bytes"
+	"testing"
+)
+
+const (
+	testSamples = 10
+)
+
+func round2(n int) int {
+	k := 1
+	for k < n {
+		k <<= 1
+	}
+	return k >> 1
+}
+
+func merkleTreeHead(d [][]byte) []byte {
+	if len(d) == 1 {
+		return d[0]
+	}
+
+	k := round2(len(d))
+	l := merkleTreeHead(d[:k])
+	r := merkleTreeHead(d[k:])
+	return pairHash(l, r)
+}
+
+func Testfrontier(t *testing.T) {
+	f := frontier{}
+	d := [][]byte{}
+
+	for i := uint64(0); i < 10; i += 1 {
+		v := []byte{byte(i)}
+		d = append(d, leafHash(v))
+		f.Add(v)
+
+		if s := f.Size(); s != i+1 {
+			t.Fatalf("Incorrect size [%d] [%d] != [%d]", i, s, i+1)
+		}
+
+		h := merkleTreeHead(d)
+		if fh := f.Head(); !bytes.Equal(h, fh) {
+			t.Fatalf("Incorrect head [%d] [%x] != [%x]", i, fh, h)
+		}
+	}
+}

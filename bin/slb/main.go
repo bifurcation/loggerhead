8f955ace8409a8331735ce30516a064f2eb55a0d
Richard Barnes <rlb@ipv.sx>
2017-03-01 10:14:54 -0500

    Add a shitty load balancer

---
+++ bin/slb/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func main() {
+	var host string
+	var portList string
+	flag.StringVar(&host, "host", "localhost", "Host to connect to")
+	flag.StringVar(&portList, "ports", "8000", "Comma-separated list of ports")
+	flag.Parse()
+
+	ports := strings.Split(",", portList)
+	hosts := make([]string, len(ports))
+	for i, port := range ports {
+		hosts[i] = fmt.Sprintf("%s:%d", host, port)
+	}
+
+	curr := 0
+	director := func(req *http.Request) {
+		log.Printf("routing request to %s", hosts[curr])
+
+		req.URL.Scheme = "http"
+		req.URL.Host = hosts[curr]
+		curr = (curr + 1) % len(hosts)
+	}
+
+	proxy := &httputil.ReverseProxy{Director: director}
+
+	log.Print("Listening...")
+	http.Handle("/", proxy)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
